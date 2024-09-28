f75d82b75f871e048c5a60f8924cc7a1991ef6a8
ykmath <matheusmesquitay@gmail.com>
2024-09-27 21:23:46 -0400

    Salvando alterações antes de voltar dois commits

--- main.js
+++ main.js
@@ -2,37 +2,12 @@ const {app, BrowserWindow, ipcMain, Menu} = require("electron");
 const path = require("path");
 const fs = require("fs");
 
-const createSplashScreen = () => {
-    const splash = new BrowserWindow({
-        width: 600,
-        height: 300,
-        frame: false,
-        show: false,
-        resizable: false,
-        alwaysOnTop: true,
-    })
-
-    splash.loadFile("./splash.html");
-
-    splash.once("ready-to-show", () => {splash.show()});
-
-    const mainApp = createWindow("index", 800, 600);
-
-    mainApp.once("ready-to-show", () => {
-        if (splash) {
-            splash.close();
-        }
-        mainApp.show();
-    })
-}
-
 const createWindow = (page, x, y) => {
     const win = new BrowserWindow({
         width: x,
         height: y,
         minWidth: 900,
         minHeight: 600,
-        show: false,
         webPreferences: {
             preload: path.join(__dirname, "preload.js")
         }
@@ -46,7 +21,7 @@ const createWindow = (page, x, y) => {
 }
 
 app.whenReady().then(() => {
-    createSplashScreen();
+    createWindow("index", 800, 600);
 
     ipcMain.on("getDados", (event, dados) => {
         const reciboPagina = createWindow("recibo", 1200, 900);
@@ -63,7 +38,7 @@ app.whenReady().then(() => {
     })
 
     app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createSplashScreen();
+        if (BrowserWindow.getAllWindows().length === 0) createWindow("index", 800, 600);
     })
 
     app.on("window-all-closed", () => {
