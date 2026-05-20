d10c5f2ac5a8ed7a097ac94404bed78360b160d0
= <matheusmesquitay@gmail.com>
2026-05-19 22:58:55 -0400

    remoção do .zip do macos

--- forge.config.js
+++ forge.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['linux', 'darwin'],
+      platforms: ['linux'],
     },
     {
       name: '@electron-forge/maker-dmg'
